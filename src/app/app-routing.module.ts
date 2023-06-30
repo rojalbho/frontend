3b6b08d7764f7162fa6c4d9af157ca37215f7a8d
R0JALBH0 <115759704+R0JALBH0@users.noreply.github.com>
2023-06-30 10:26:18 -0400

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { ServicesComponent } from './components/pages/services/services.component';
+import { CareersComponent } from './components/pages/careers/careers.component';
+import { LoginComponent } from './components/pages/login/login.component';
+
+
+const routes: Routes = [
+  {
+    path:'',component:HomeComponent 
+  },
+  {
+    path:'about',component:AboutComponent
+  },
+  {
+    path:'contact-us',component:ContactComponent
+  },
+  {
+    path:'services',component:ServicesComponent
+  },
+  {
+    path:'careers', component: CareersComponent
+  },
+  {
+    path:'login', component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PreloaderComponent } from './components/layouts/preloader/preloader.component';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ServicesComponent } from './components/pages/services/services.component';
+import { TeamComponent } from './components/pages/team/team.component';
+import { TeamdetailComponent } from './components/pages/teamdetail/teamdetail.component';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/pages/login/login.component';
+import { CareersComponent } from './components/pages/careers/careers.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PreloaderComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    ServicesComponent,
+    TeamComponent,
+    TeamdetailComponent,
+    LoginComponent,
+    CareersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
